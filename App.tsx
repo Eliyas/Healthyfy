9ef38d632d21533df087506609c1aed4831c3795
saifeliyas <saifeliyas@gmail.com>
2023-12-08 00:33:26 +0530

    navigation related changes

--- App.tsx
+++ App.tsx
@@ -1,31 +1,18 @@
-import { View } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import RootNavigation from './navigation/RootNavigation';
 import styled from 'styled-components/native';
 import { Amplify } from 'aws-amplify';
 import amplifyconfig from './src/amplifyconfiguration.json';
-import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from './screens/home/HomeScreen';
-import UCLoggingScreen from './screens/uc_logging/UCLoggingScreen';
-import { TransitionPresets } from '@react-navigation/stack';
 Amplify.configure(amplifyconfig);
 
 
-const Stack = createNativeStackNavigator();
-
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{
-          headerShown: false,
-          ...TransitionPresets.SlideFromRightIOS,
-          gestureEnabled: true,
-          gestureDirection: "horizontal",
-        }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="UCLogging" component={UCLoggingScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-
+    <Container>
+    <StatusBar style="auto" />
+    <RootNavigation />
+  </Container>
   );
 }
 
